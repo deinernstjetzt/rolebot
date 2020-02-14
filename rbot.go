c51035d45ac27d8a7c3e0123d9b9427ac3650c84
deinernstjetzt <fabian.vanrissenbeck@gmail.com>
2020-02-14 19:28:07 +0100

    Simplified a lot of code

--- rbot.go
+++ rbot.go
@@ -1,373 +1,367 @@
 package main
 
 import (
-  "os"
-  "io/ioutil"
-  "fmt"
-  "encoding/json"
-  "github.com/bwmarrin/discordgo"
-  "syscall"
-  "os/signal"
-  "strings"
+	"encoding/json"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 )
 
 type Config struct {
-  Token string `json:"token"`
-  Game string `json:"game"`
-  Server []ConfigServer `json:"server"`
+	Token      string         `json:"token"`
+	Game       string         `json:"game"`
+	Server     []ConfigServer `json:"server"`
+	ConfigFile string
 }
 
 type ConfigServer struct {
-  ServerID string `json:"serverid"`
-  ChannelID string `json:"channelid"`
-  Admin string `json:"adminid"`
-  SecondaryAdmins []string `json:"secondary_admins"`
-  Roles []ConfigRoles `json:"roles"`
+	ServerID        string        `json:"serverid"`
+	ChannelID       string        `json:"channelid"`
+	Admin           string        `json:"adminid"`
+	SecondaryAdmins []string      `json:"secondary_admins"`
+	Roles           []ConfigRoles `json:"roles"`
 }
 
 type ConfigRoles struct {
-  Emoji string `json:"emoji"`
-  Role string `json:"role"`
+	Emoji string `json:"emoji"`
+	Role  string `json:"role"`
 }
 
 var config Config
+
 const configFile = "rbot.json"
 
 func loadConfig() {
-  file, err := os.Open(configFile)
+	file, err := os.Open(configFile)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  defer file.Close()
-  buf, err2 := ioutil.ReadAll(file)
+	defer file.Close()
+	buf, err2 := ioutil.ReadAll(file)
 
-  if err2 != nil {
-    panic(err2)
-  }
+	if err2 != nil {
+		panic(err2)
+	}
 
-  json.Unmarshal(buf, &config)
+	config.ConfigFile = configFile
+	json.Unmarshal(buf, &config)
 }
 
 func (c ConfigServer) isAdmin(userID string) bool {
-  if c.Admin == userID {
-    return true
-  }
+	if c.Admin == userID {
+		return true
+	}
 
-  for _, admin := range c.SecondaryAdmins {
-    if userID == admin  {
-      return true
-    }
-  }
+	for _, admin := range c.SecondaryAdmins {
+		if userID == admin {
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
-
 func main() {
-  loadConfig()
-  discord, _ := bot_main()
-  defer discord.Close()
-  sc := make(chan os.Signal, 1)
+	loadConfig()
+	discord, _ := bot_main()
+	defer discord.Close()
+	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 }
 
 func bot_main() (d *discordgo.Session, e error) {
-  discord, err := discordgo.New("Bot " + config.Token)
-  if err != nil {
-    fmt.Println("Failed to connect to discord api")
-    e = err
-    return
-  }
+	discord, err := discordgo.New("Bot " + config.Token)
+	if err != nil {
+		fmt.Println("Failed to connect to discord api")
+		e = err
+		return
+	}
 
-  discord.AddHandler(messageCreate)
-  discord.AddHandler(reactionAdd)
-  discord.AddHandler(reactionRemove)
+	discord.AddHandler(messageCreate)
+	discord.AddHandler(reactionAdd)
+	discord.AddHandler(reactionRemove)
 
-  err = discord.Open()
-  if err != nil {
-    fmt.Println("Failed to connect to discord api")
-    e = err
-    return
-  }
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("Failed to connect to discord api")
+		e = err
+		return
+	}
 
-  fmt.Println("Bot is booted up and ready!")
-  d = discord
-  e = nil
-  return
+	fmt.Println("Bot is booted up and ready!")
+	d = discord
+	e = nil
+	return
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-  if m.Content == "ping" {
-    s.ChannelMessageSend(m.ChannelID, "pong!")
-  }
+	if m.Content == "!addserver" {
+		config.addServer(m.Message)
+	}
 
-  if m.Content == "!addserver" {
-    for _, server := range config.Server {
-      if server.ServerID == m.GuildID {
-        fmt.Println("Already added!")
-        _, err := s.ChannelMessageSend(m.ChannelID, "This server has already been added!")
-        if err != nil {
-          fmt.Println(err)
-        }
-        return
-      }
-    }
+	if strings.Index(m.Content, "!addrole") == 0 {
+		config.addRoleToServer(m.Message)
+	}
 
-    newconfig := config
-    server := ConfigServer {
-      ServerID: m.GuildID,
-      ChannelID: m.ChannelID,
-      Roles: make([]ConfigRoles, 1),
-      Admin: m.Author.ID,
-    }
+	if strings.Index(m.Content, "!remrole") == 0 {
+		config.removeRoleFromServer(m.Message)
+	}
 
-    newconfig.Server = append(newconfig.Server, server)
-    updateConfig(newconfig)
-    fmt.Printf("Added server %v to rbot config\n", m.GuildID)
-    s.ChannelMessageSend(m.ChannelID, "This server has been successfully added")
-  }
+	if strings.Index(m.Content, "!addadmin") == 0 {
+		id, err := extractUserId(m.Message)
+		server := config.getServer(m.GuildID)
 
-  if strings.Index(m.Content, "!addrole") == 0 {
-    //fmt.Println(extractEmojiName(m.Message))
-    addRoleToServer(m.Message)
-  }
+		if server == nil || err != nil {
+			return
+		}
 
-  if strings.Index(m.Content, "!remrole") == 0 {
-    fmt.Println("yoink")
-    removeRoleFromServer(m.Message)
-  }
+		server.addSecondaryAdmin(id)
+		config.updateConfig()
+	}
 
-  if strings.Index(m.Content, "!addadmin") == 0 {
-    id, err := extractUserId(m.Message)
-    if err == nil {
-      for i, server := range config.Server {
-        if server.ServerID == m.GuildID && server.isAdmin(m.Author.ID) {
-          config.Server[i].addSecondaryAdmin(id)
-          updateConfig(config)
-        }
-      }
-    }
-  }
+	if strings.Index(m.Content, "!remadmin") == 0 {
+		id, err := extractUserId(m.Message)
+		server := config.getServer(m.GuildID)
 
-  if strings.Index(m.Content, "!remadmin") == 0 {
-    id, err := extractUserId(m.Message)
-    if err == nil {
-      for i, server := range config.Server {
-        if server.ServerID == m.GuildID && server.isAdmin(m.Author.ID) {
-          config.Server[i].removeSecondaryAdmin(id)
-          updateConfig(config)
-        }
-      }
-    }
-  }
+		if server == nil || err != nil {
+			return
+		}
+
+		server.removeSecondaryAdmin(id)
+		config.updateConfig()
+	}
+}
+
+func (c *Config) addServer(m *discordgo.Message) {
+	if config.getServer(m.GuildID) != nil {
+		return //Dont add server that already exist
+	}
+
+	server := ConfigServer{
+		ServerID:  m.GuildID,
+		ChannelID: m.ChannelID,
+		Roles:     make([]ConfigRoles, 1),
+		Admin:     m.Author.ID,
+	}
+
+	c.Server = append(c.Server, server)
+	c.updateConfig()
+	fmt.Printf("Server %v has been added to this bot.\n", server.ServerID)
 }
 
 func reactionAdd(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
-  for _, server := range config.Server {
-    if server.ServerID == m.GuildID && server.ChannelID == m.ChannelID {
-      for _, role := range server.Roles {
-        if role.Emoji == m.Emoji.Name {
-          fmt.Printf("Adding role %v to user %v\n", role.Role, m.UserID)
-          err := s.GuildMemberRoleAdd(m.GuildID, m.UserID, role.Role)
+	server := config.getServer(m.GuildID)
 
-          if err != nil {
-            fmt.Println(err)
-          }
-        }
-      }
-    }
-  }
+	if server == nil {
+		return
+	}
+
+	if server.ChannelID != m.ChannelID {
+		return
+	}
+
+	for _, role := range server.Roles {
+		if role.Emoji == m.Emoji.Name {
+			fmt.Printf("Adding role %v to user %v\n", role.Role, m.UserID)
+			err := s.GuildMemberRoleAdd(m.GuildID, m.UserID, role.Role)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
 
 func reactionRemove(s *discordgo.Session, m *discordgo.MessageReactionRemove) {
-  for _, server := range config.Server {
-    if server.ServerID == m.GuildID && server.ChannelID == m.ChannelID {
-      for _, role := range server.Roles {
-        if role.Emoji == m.Emoji.Name {
-          fmt.Printf("Removing role %v from user %v\n", role.Role, m.UserID)
-          err := s.GuildMemberRoleRemove(m.GuildID, m.UserID, role.Role)
+	server := config.getServer(m.GuildID)
 
-          if err != nil {
-            fmt.Println(err)
-          }
-        }
-      }
-    }
-  }
+	if server == nil {
+		return
+	}
+
+	if server.ChannelID != m.ChannelID {
+		return
+	}
+
+	for _, role := range server.Roles {
+		if role.Emoji == m.Emoji.Name {
+			fmt.Printf("Removing role %v from user %v\n", role.Role, m.UserID)
+			err := s.GuildMemberRoleRemove(m.GuildID, m.UserID, role.Role)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
 
-func updateConfig(newconfig Config) {
-  data, err := json.Marshal(newconfig)
+func (c Config) updateConfig() {
+	data, err := json.Marshal(c)
 
-  if err != nil {
-    fmt.Printf("Failed to update config: %v\n", err)
-  }
+	if err != nil {
+		fmt.Printf("Failed to update config: %v\n", err)
+	}
 
-  file, err2 := os.Create(configFile)
-  defer file.Close()
+	file, err2 := os.Create(c.ConfigFile)
+	defer file.Close()
 
-  if err2 != nil {
-    fmt.Printf("Can't replace config file: %v\n", err2)
-  }
+	if err2 != nil {
+		fmt.Printf("Can't replace config file: %v\n", err2)
+	}
 
-  _, err2 = file.Write(data)
+	_, err2 = file.Write(data)
 
-  if err2 != nil {
-    fmt.Printf("Can't write to config file: %v\n", err2)
-  }
-
-  config = newconfig
+	if err2 != nil {
+		fmt.Printf("Can't write to config file: %v\n", err2)
+	}
 }
 
-type GenericError struct {}
+func (c *Config) getServer(id string) *ConfigServer {
+	for i, server := range c.Server {
+		if server.ServerID == id {
+			return &c.Server[i]
+		}
+	}
+	return nil
+}
+
+type GenericError struct{}
+
 func (g GenericError) Error() string {
-  return "error"
+	return "error"
 }
 
-func getRoleId(message *discordgo.Message) (s string, e error) {
-  i := strings.Index(message.Content, "<@&")
+func extractRoleId(message *discordgo.Message) (s string, e error) {
+	i := strings.Index(message.Content, "<@&")
 
-  if i == -1 {
-    e = GenericError{}
-    return
-  }
+	if i == -1 {
+		e = GenericError{}
+		return
+	}
 
-  slice := message.Content[i+3:]
-  j := strings.Index(slice, ">")
+	slice := message.Content[i+3:]
+	j := strings.Index(slice, ">")
 
-  if j == -1 {
-    e = GenericError{}
-    return
-  }
+	if j == -1 {
+		e = GenericError{}
+		return
+	}
 
-  s = slice[:j]
-  return
+	s = slice[:j]
+	return
 }
 
 func extractUserId(message *discordgo.Message) (s string, e error) {
-  i := strings.Index(message.Content, "<@!")
+	i := strings.Index(message.Content, "<@!")
 
-  if i == -1 {
-    e = GenericError{}
-    return
-  }
+	if i == -1 {
+		e = GenericError{}
+		return
+	}
 
-  slice := message.Content[i+3:]
-  j := strings.Index(slice, ">")
+	slice := message.Content[i+3:]
+	j := strings.Index(slice, ">")
 
-  if j == -1 {
-    e = GenericError{}
-    return
-  }
+	if j == -1 {
+		e = GenericError{}
+		return
+	}
 
-  s = slice[:j]
-  return
+	s = slice[:j]
+	return
 }
 
 func extractEmojiName(message *discordgo.Message) (s string, e error) {
-  i := strings.Index(message.Content, "<:")
+	i := strings.Index(message.Content, "<:")
 
-  if i == -1 {
-    e = GenericError {}
-    return
-  }
+	if i == -1 {
+		e = GenericError{}
+		return
+	}
 
-  slice := message.Content[i+2:]
-  j := strings.Index(slice, ":")
+	slice := message.Content[i+2:]
+	j := strings.Index(slice, ":")
 
-  if j == -1 {
-    e = GenericError {}
-    return
-  }
+	if j == -1 {
+		e = GenericError{}
+		return
+	}
 
-  s = slice[:j]
-  return
+	s = slice[:j]
+	return
 }
 
-func addRoleToServer(message *discordgo.Message) error {
-  id, err := getRoleId(message)
+func (c *Config) addRoleToServer(message *discordgo.Message) error {
+	id, err := extractRoleId(message)
+	server := c.getServer(message.GuildID)
+	emoji, err2 := extractEmojiName(message)
 
-  if err != nil {
-    return err
-  }
+	if err != nil || err2 != nil || server == nil {
+		return GenericError{}
+	}
 
-  emoji, err2 := extractEmojiName(message)
+	roles := ConfigRoles{
+		Emoji: emoji,
+		Role:  id,
+	}
 
-  if err2 != nil {
-    return err
-  }
+	if server.isAdmin(message.Author.ID) {
+		server.Roles = append(server.Roles, roles)
+		c.updateConfig()
+	}
 
-  roles := ConfigRoles {
-    Emoji: emoji,
-    Role: id,
-  }
-
-  fmt.Println(roles)
-
-  newconfig := config
-  for i, server := range newconfig.Server {
-    if server.ServerID == message.GuildID {
-      if server.isAdmin(message.Author.ID) {
-        newconfig.Server[i].Roles = append(newconfig.Server[i].Roles, roles)
-        updateConfig(newconfig)
-      } else {
-        fmt.Println("permission denied")
-      }
-    }
-  }
-
-  return nil
+	return nil
 }
 
-func arrayRemove(arr []ConfigRoles, index int) []ConfigRoles {
-  arr[index], arr[len(arr) - 1] = arr[len(arr) - 1], arr[index]
-  return arr[:len(arr) - 1]
-}
+func (c *Config) removeRoleFromServer(message *discordgo.Message) error {
+	id, err := extractRoleId(message)
+	server := c.getServer(message.GuildID)
 
-func removeRoleFromServer(message *discordgo.Message) error {
-  id, err := getRoleId(message)
+	if err != nil || server == nil {
+		return GenericError{}
+	}
 
-  if err != nil {
-    return err
-  }
+	if !server.isAdmin(message.Author.ID) {
+		fmt.Println("Permission denied")
+		return GenericError{}
+	}
 
-  newconfig := config
-  for i, server := range newconfig.Server {
-    if server.ServerID == message.GuildID {
-      if !server.isAdmin(message.Author.ID) {
-        fmt.Println("Permission denied")
-        return GenericError {}
-      }
-      for j, role := range server.Roles {
-        if role.Role == id {
-          fmt.Printf("Deleting %v from server\n", id)
-          newconfig.Server[i].Roles = arrayRemove(newconfig.Server[i].Roles, j)
-          updateConfig(newconfig)
-        }
-      }
-    }
-  }
+	for j, role := range server.Roles {
+		if role.Role == id {
+			fmt.Printf("Deleting role %v from server\n", id)
+			server.Roles[len(server.Roles)-1], server.Roles[j] =
+				server.Roles[j], server.Roles[len(server.Roles)-1]
 
-  return nil
+			server.Roles = server.Roles[:len(server.Roles)-1]
+			c.updateConfig()
+			return nil
+		}
+	}
+
+	return GenericError{}
 }
 
 func (c *ConfigServer) addSecondaryAdmin(userID string) error {
-  c.SecondaryAdmins = append(c.SecondaryAdmins, userID)
-  return nil
+	c.SecondaryAdmins = append(c.SecondaryAdmins, userID)
+	return nil
 }
 
 func (c *ConfigServer) removeSecondaryAdmin(userID string) error {
-  for i, id := range c.SecondaryAdmins {
-    if id == userID {
-      c.SecondaryAdmins[i], c.SecondaryAdmins[len(c.SecondaryAdmins) - 1] =
-          c.SecondaryAdmins[len(c.SecondaryAdmins) - 1], c.SecondaryAdmins[i]
+	for i, id := range c.SecondaryAdmins {
+		if id == userID {
+			c.SecondaryAdmins[i], c.SecondaryAdmins[len(c.SecondaryAdmins)-1] =
+				c.SecondaryAdmins[len(c.SecondaryAdmins)-1], c.SecondaryAdmins[i]
 
-      c.SecondaryAdmins = c.SecondaryAdmins[:len(c.SecondaryAdmins) - 1]
-    }
-  }
+			c.SecondaryAdmins = c.SecondaryAdmins[:len(c.SecondaryAdmins)-1]
+		}
+	}
 
-  return nil
+	return nil
 }
